4bdfac120d86217efb6871116e25f3f5bbb4d945
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-10 01:43:18 +0600

    added auth state change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle}=useFirebase()
     return (
         <div>
             <h2>Please Login</h2>
+            <div style={{margin:'5px'}}>
+                <button onClick={signInWithGoogle}>Sigin Google</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Type your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Type password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>Please Register now</h2>
+            <form>
+                <input type="text" name="" id="" placeholder='Type your name'/>
+                <br />
+                <input type="email" name="" id="" placeholder='Type your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Type password' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
